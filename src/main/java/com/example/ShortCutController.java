4d0fdf259b7135d3cebfeab50a294d0c175ad2d8
d.kanehira <disc99.g@gmail.com>
2016-07-17 20:28:51 +0900

    Add short cut handling

--- src/main/java/com/example/ShortCutController.java
+++ src/main/java/com/example/ShortCutController.java
@@ -2,12 +2,21 @@ package com.example;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ShortCutController {
+
     @GetMapping("/")
     String index() {
         return "index";
+    }
+
+    @PostMapping("/shortcut")
+    String shortcut(@RequestParam String url) {
+        System.out.println(url);
+        return "result";
     }
 
 }
